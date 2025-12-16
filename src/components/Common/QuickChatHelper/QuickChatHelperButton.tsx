e3640b0904a9de9a17ee51570a9227269ca97c2f
Robert <contact@rmusser.net>
2025-12-16 01:14:44 -0800

    quick chat toggle

--- src/components/Common/QuickChatHelper/QuickChatHelperButton.tsx
+++ src/components/Common/QuickChatHelper/QuickChatHelperButton.tsx
@@ -40,7 +40,7 @@ export const QuickChatHelperButton: React.FC = () => {
 
   return (
     <>
-      <div className="fixed bottom-6 right-6 z-[60] flex items-center gap-3">
+      <div className="fixed bottom-6 right-6 z-[60] flex flex-col items-center gap-3">
         {!hideQuickChatHelper && (
           <Tooltip title={tooltip} placement="left">
             <button
